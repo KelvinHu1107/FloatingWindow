925b0dfe76442dd003ef48637e6154f5f37e0c29
KINGWAY\kelvinhu1107 <kelvinhu1107@kingwaytek.com>
2017-11-06 11:25:43 +0800

    Refine content

--- app/src/main/java/com/develoment/kelvin/floatingwindow/FloatWindowService.java
+++ app/src/main/java/com/develoment/kelvin/floatingwindow/FloatWindowService.java
@@ -1,10 +1,13 @@
 package com.develoment.kelvin.floatingwindow;
 
+import android.annotation.TargetApi;
 import android.app.Service;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
@@ -22,48 +25,70 @@ import android.widget.TextView;
 public class FloatWindowService extends Service {
 
     WindowManager windowManager;
-    LinearLayout linearLayout;
+    LinearLayout currentSpeed, speedLimit;
+    Button button;
 
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onCreate() {
         super.onCreate();
 
+        initComponent();
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void initComponent(){
+
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        linearLayout = new LinearLayout(this);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
-        linearLayout.setBackgroundColor(Color.argb(66, 255, 0, 0));
-        linearLayout.setLayoutParams(layoutParams);
-        TextView textView = new TextView(this);
-        textView.setText("This is a floating window");
-        textView.setTextColor(Color.BLUE);
-        textView.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
-        final Button button = new Button(this);
+        currentSpeed = new LinearLayout(this);
+        speedLimit = new LinearLayout(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        button = new Button(this);
         button.setText("Close");
         button.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
         LinearLayout.LayoutParams layoutParamsText = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
-// linearLayout.setBackgroundColor(Color.argb(66, 255, 0, 0));
+        // linearLayout.setBackgroundColor(Color.argb(66, 255, 0, 0));
+
+        currentSpeed.setBackgroundColor(Color.argb(66, 255, 0, 0));
+        currentSpeed.setLayoutParams(layoutParams);
+        currentSpeed.setOrientation(LinearLayout.VERTICAL);
+        speedLimit.setBackground(getResources().getDrawable(R.mipmap.ic_launcher));
+
+        final TextView textView = new TextView(this);
+        textView.setText("0.0 Km/h");
+        textView.setTextColor(Color.BLUE);
+        textView.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+
         textView.setLayoutParams(layoutParamsText);
 
-        linearLayout.addView(textView);
-        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(400,150,WindowManager.LayoutParams.TYPE_PHONE,WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+        currentSpeed.addView(speedLimit);
+        currentSpeed.addView(textView);
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(400,500,WindowManager.LayoutParams.TYPE_PHONE,WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
         params.x = 0;
         params.y = 0;
         params.gravity = Gravity.RIGHT;
         windowManager.addView(button, params);
         params.gravity = Gravity.CENTER | Gravity.CENTER;
-        windowManager.addView(linearLayout, params);
+        windowManager.addView(currentSpeed, params);
 
+        setClickListener(params);
+        task(textView);
+    }
+
+    private void setClickListener(final WindowManager.LayoutParams params){
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                windowManager.removeViewImmediate(linearLayout);
+                windowManager.removeViewImmediate(currentSpeed);
                 windowManager.removeViewImmediate(button);
                 stopSelf();
             }
         });
 
-        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+        currentSpeed.setOnTouchListener(new View.OnTouchListener() {
             WindowManager.LayoutParams updatedParams = params;
             int x,y;
             float touchX,touchY;
@@ -79,22 +104,35 @@ public class FloatWindowService extends Service {
                     case MotionEvent.ACTION_MOVE:
                         updatedParams.x = (int)(x+(motionEvent.getRawX() - touchX));
                         updatedParams.y = (int)(y+(motionEvent.getRawY() - touchY));
-                        windowManager.updateViewLayout(linearLayout,updatedParams);
+                        windowManager.updateViewLayout(currentSpeed,updatedParams);
                         break;
-
-                    case MotionEvent.ACTION_BUTTON_PRESS:
-                        windowManager.removeViewImmediate(linearLayout);
-
                     default:break;
                 }
 
                 return false;
             }
 
-
-
         });
+    }
 
+    private void task(final TextView textView){
+        final Runnable mUpdateUITimerTask = new Runnable() {
+            public void run() {
+                // do whatever you want to change here, like:
+                textView.setText("100 Km/h");
+            }
+        };
+
+        final Runnable mUpdateUITimerTask2 = new Runnable() {
+            public void run() {
+                // do whatever you want to change here, like:
+                textView.setText("80 Km/h");
+            }
+        };
+        final Handler mHandler = new Handler();
+
+        mHandler.postDelayed(mUpdateUITimerTask, 3 * 1000);
+        mHandler.postDelayed(mUpdateUITimerTask2, 4 * 1000);
     }
 
     @Nullable
